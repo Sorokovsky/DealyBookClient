6fb94b019d6be8a49b0b1f0b6345432025d77e84
Sorokovsky <Sorokovskys@ukr.net>
2023-01-26 20:29:48 +0300

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NextPage } from 'next';
+ const Home:NextPage = ():JSX.Element => {
+  return (
+    <>
+      
+    </>
+  )
+}
+export default React.memo(Home);
