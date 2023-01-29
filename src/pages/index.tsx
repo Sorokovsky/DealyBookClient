9c208089061caec1ee917b3a6925a3a097a1fe73
Sorokovsky <Sorokovskys@ukr.net>
2023-01-29 21:36:47 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NextPage } from 'next';
 import MainLayout from '@/components/MainLayout';
- const Home:NextPage = React.memo(():JSX.Element => {
+ const Home:NextPage = ():JSX.Element => {
   return (
     <MainLayout title='Головна'>
     </MainLayout>
   )
-})
+}
 export default Home;
