052c461219905d6c60dbbc3ff583a7a24517a65c
Sorokovsky <Sorokovskys@ukr.net>
2023-02-02 19:39:37 +0300

    fix

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -17,7 +17,7 @@ const MainLayout:FC<IProps> = memo(({title, children, description, keywords}):JS
         <>  
         <Head>
             <meta charSet="UTF-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="description" content={`DealyBook-це ідеальний сервіс для зберігання важливої інформації. ${description ? description : ''}`} />
             <meta name="keywords" content={`DealyBook, конспект, зберігання, сховище ${keywords ? ',' + keywords : ''}`} />
